6f24e2bc271bb57b045370b9a3c5ff26dc54d017
Ron Liniix <teedev.tee@gmail.com>
2021-10-06 11:16:14 +0700

    buffer delete

--- init.lua
+++ init.lua
@@ -111,7 +111,7 @@ vim.api.nvim_set_keymap('n', '<A-c>', ':BufferClose<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<A-s>', ':BufferLinePick<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<A->>', ':BufferLineMoveNext<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<A-<>', ':BufferLineMovePrev<CR>', { silent = true })
-vim.api.nvim_set_keymap('n', '<A-q>', ':bdelete<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<A-w>', ':bdelete<CR>', { silent = true })
 
 --File Browser
 vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
