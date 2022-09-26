3854b742d7cf75ff9ab4eb8c46239f5e6653564f
Ron Liniix <teedev.tee@gmail.com>
2022-09-26 23:28:53 +0700

    remove defaule lualine config

--- init.lua
+++ init.lua
@@ -145,29 +145,11 @@ require('lualine').setup {
 	options = {
 		icons_enabled = true,
 		theme = 'auto',
-		component_separators = { left = '', right = ''},
-		section_separators = { left = '', right = ''},
-		disabled_filetypes = {},
-		always_divide_middle = true,
 	},
 	sections = {
-		lualine_a = {'mode'},
 		lualine_b = {'branch', 'diff', 'FugitiveHead'},
-		lualine_c = {'filename'},
-		lualine_x = {'encoding', 'fileformat', 'filetype'},
-		lualine_y = {'progress'},
-		lualine_z = {'location'}
+		lualine_c = {{'filename', path = 3 }},
 	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = {'filename'},
-		lualine_x = {'location'},
-		lualine_y = {},
-		lualine_z = {}
-	},
-	tabline = {},
-	extensions = {}
 }
 
 -- Tabline Config
@@ -368,10 +350,8 @@ require('telescope').setup({
 				keep_insert = true,
 			},
 			media_files = {
-				-- filetypes whitelist
-				-- defaults to {'png', 'jpg', 'mp4', 'webm', 'pdf'}
 				filetypes = {'png', 'webp', 'jpg', 'jpeg'},
-				find_cmd = 'rg' -- find command (defaults to `fd`)
+				find_cmd = 'rg'
 			},
 		},
 	}
