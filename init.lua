9a6e3c067b3cacb362e44774a43af6c266700872
Ron Liniix <teedev.tee@gmail.com>
2022-09-27 13:53:08 +0700

    config cokeline for NVimTree

--- init.lua
+++ init.lua
@@ -167,8 +167,22 @@ require('lualine').setup {
 }
 
 --CokeLine
+local get_hex = require('cokeline/utils').get_hex
+local yellow = vim.g.terminal_color_3
+
 require('cokeline').setup({
 	buffers = {
+	},
+	sidebar = {
+		filetype = 'NvimTree',
+		components = {
+			{
+				text = '  NvimTree',
+				fg = yellow,
+				bg = get_hex('NvimTreeNormal', 'bg'),
+				style = 'bold',
+			},
+		},
 	},
 	components = {
 		{
