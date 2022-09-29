a56b55f033056eb82f27781457e3db9edafbe2f2
Ron Liniix <teedev.tee@gmail.com>
2022-09-29 11:57:00 +0700

    lualine: change inactive filename to full path

--- init.lua
+++ init.lua
@@ -158,14 +158,21 @@ require('colorizer').setup()
 
 -- Status Bar
 require('lualine').setup {
+	extensions = { 
+		'toggleterm',
+		'nvim-tree',
+	},
 	options = {
 		icons_enabled = true,
 		theme = 'auto',
 	},
 	sections = {
-		lualine_b = {'branch', 'diff', 'FugitiveHead'},
+		lualine_b = {'branch', 'diff'},
 		lualine_c = {{'filename', path = 3 }},
 	},
+	inactive_sections = {
+		lualine_c = {{'filename', path = 3 }},
+	}
 }
 
 --CokeLine
