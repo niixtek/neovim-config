a3483a58c1480372a0657e7d6231b5bdbecc760f
Ron Liniix <teedev.tee@gmail.com>
2023-02-16 17:32:27 +0700

    remove default config

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,22 +1,5 @@
 --ToggleTerm
 require('toggleterm').setup({
   open_mapping = [[<c-\>]],
-  hide_numbers = true, 
-  shade_filetypes = {},
-  shade_terminals = true,
-  shading_factor = '1', 
-  start_in_insert = true,
-  insert_mappings = true, 
-  persist_size = true,
   direction = 'float',
-  close_on_exit = true, 
-  shell = vim.o.shell,
-  float_opts = {
-    border =  'curved',
-    winblend = 0,
-    highlights = {
-      border = 'Normal',
-      background = 'Normal',
-    }
-  }
 })
