8946ced3fd6384083371b35f3386cdc2d38636b7
Ron Liniix <teedev.tee@gmail.com>
2023-02-20 17:26:31 +0700

    disable session temporary

--- lua/plugins/workspace.lua
+++ lua/plugins/workspace.lua
@@ -2,20 +2,20 @@
 require('workspaces').setup({
 	hooks = {
 		open = {
-			--'Telescope find_files',
-			function()
+			'Telescope find_files',
+--[[ 			function()
 				require('sessions').load(nil, { silent = true })
-			end,
+			end, ]]
 		},
 	}
 })
 
 --Session
-require('sessions').setup({
-	events = { "WinEnter" },
+--[[ require('sessions').setup({
+	events = { 'VimLeavePre' },
 	session_filepath = vim.fn.stdpath("data") .. "/sessions",
 	absolute = true,
---[[ 	hooks = {
+	hooks = {
 		open = { 'Telescope find_files' },
-	} ]]
-})
+	}
+}) ]]
